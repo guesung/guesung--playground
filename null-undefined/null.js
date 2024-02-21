175d1ba8325e5d7b8151d88c7737210201632c9c
Kuesung Park <gueit214@naver.com>
2024-02-21 16:35:42 +0900

    feat(null-undefined) : null과 undefined를 테스트한다

---
+++ null-undefined/null.js
@@ -0,0 +1,6 @@
+let b = null;
+
+console.log(b); // null
+console.log(typeof b); // object
+console.log(b + 1); // 1
+console.log(!b); // true
---
+++ null-undefined/undefined.js
@@ -0,0 +1,10 @@
+let a;
+
+console.log(a); // undefined
+console.log(typeof a); // undefined
+console.log(a + 1); // NaN
+console.log(!a); // true
+
+console.log(undefined == null); // true
+console.log(undefined === null); // false
+console.log(undefined !== null); // true
