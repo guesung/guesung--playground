d6a6dc712a0ff07d98e13f36651017f74cce453d
Kuesung Park <gueit214@naver.com>
2024-02-21 13:33:13 +0900

    refactor(react-query) : react-query로 교체한다

--- udemy-REACT-QUERY/base-blog-em/src/App.jsx
+++ udemy-REACT-QUERY/base-blog-em/src/App.jsx
@@ -1,13 +1,19 @@
 import { Posts } from "./Posts";
 import "./App.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 function App() {
+  const queryClient = new QueryClient({
+    defaultOptions: {},
+  });
+
   return (
-    // provide React Query client to App
-    <div className="App">
-      <h1>Blog &apos;em Ipsum</h1>
-      <Posts />
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className="App">
+        <h1>Blog &apos;em Ipsum</h1>
+        <Posts />
+      </div>
+    </QueryClientProvider>
   );
 }
 
