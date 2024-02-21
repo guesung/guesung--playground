7680ffd8f7c9f7ba48a9ca5d4dee96c36d2a5b9f
Kuesung Park <gueit214@naver.com>
2024-02-21 15:11:03 +0900

    feat(typeof) : typeof를 테스트한다

---
+++ typeof/index.js
@@ -0,0 +1,15 @@
+//  원시 타입
+console.log(typeof "문자열");
+console.log(typeof true);
+console.log(typeof 1);
+console.log(typeof BigInt(1));
+console.log(typeof Symbol("id"));
+console.log(typeof null);
+console.log(typeof undefined);
+
+// 객체 타입
+console.log(typeof new Number(1));
+console.log(typeof new String(1));
+
+console.log(typeof function () {});
+console.log(typeof (() => {}));
